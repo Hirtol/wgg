624c647b3b08e70cac9d442d6051ea6b42b59d87
Valentijn Hol <valentijnhol@hotmail.nl>
2022-08-31 23:39:46 +0200

    Add diesel & co

---
+++ migrations/2022-08-31-213053_create_system_tables/up.sql
@@ -0,0 +1,28 @@
+-- Aggregate Ingredients
+CREATE TABLE IF NOT EXISTS agg_ingredients
+(
+    id         INTEGER PRIMARY KEY NOT NULL,
+    name       TEXT                NOT NULL,
+    created_by INTEGER             NOT NULL,
+    created_at DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (created_by) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS agg_ingredients_link
+(
+    id               INTEGER NOT NULL,
+    provider_id      INTEGER NOT NULL,
+    -- Provider specific ingredient Id
+    provider_ingr_id TEXT    NOT NULL,
+
+    PRIMARY KEY (id, provider_id),
+    FOREIGN KEY (id) REFERENCES agg_ingredients (id) ON DELETE CASCADE,
+    FOREIGN KEY (provider_id) REFERENCES providers (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS providers
+(
+    id   INTEGER PRIMARY KEY NOT NULL,
+    name TEXT                NOT NULL
+);
---
+++ wgg_http/src/schema.rs
@@ -0,0 +1,58 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    agg_ingredients (id) {
+        id -> Integer,
+        name -> Text,
+        created_by -> Integer,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    agg_ingredients_link (id, provider_id) {
+        id -> Integer,
+        provider_id -> Integer,
+        provider_ingr_id -> Text,
+    }
+}
+
+diesel::table! {
+    providers (id) {
+        id -> Integer,
+        name -> Text,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Integer,
+        email -> Text,
+        username -> Text,
+        hash -> Text,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    users_tokens (id) {
+        id -> Integer,
+        user_id -> Integer,
+        token -> Text,
+        created -> Timestamp,
+        expires -> Timestamp,
+    }
+}
+
+diesel::joinable!(agg_ingredients -> users (created_by));
+diesel::joinable!(agg_ingredients_link -> agg_ingredients (id));
+diesel::joinable!(agg_ingredients_link -> providers (provider_id));
+diesel::joinable!(users_tokens -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    agg_ingredients,
+    agg_ingredients_link,
+    providers,
+    users,
+    users_tokens,
+);
