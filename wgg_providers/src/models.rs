55f143de7596947b1c9738eaf80584259e5c72fe
Valentijn Hol <valentijnhol@hotmail.nl>
2022-09-05 22:42:05 +0200

    Define a full product model

--- wgg_providers/src/models.rs
+++ wgg_providers/src/models.rs
@@ -122,3 +122,84 @@ pub enum UnavailableReason {
     TemporarilyUnavailable,
     Unknown,
 }
+
+// ** Full Product **
+#[derive(Serialize, Deserialize, async_graphql::SimpleObject, Clone, Debug, PartialEq, PartialOrd)]
+pub struct Product {
+    /// This service's ID for the current product.
+    /// Not transferable between [Provider]s
+    pub id: String,
+    /// The name of the product.
+    pub name: String,
+    /// Full product description.
+    pub description: String,
+    /// Any additional info relevant for the consumer.
+    pub additional_info: Option<String>,
+    /// The full price of an article, ignoring any sales
+    pub full_price: CentPrice,
+    /// The present display price (taking into account active sales).
+    pub display_price: CentPrice,
+    /// The amount of weight/liters/pieces this product represents.
+    pub unit_quantity: UnitQuantity,
+    pub unit_price: Option<UnitPrice>,
+    /// A small check to see if the current item is unavailable.
+    ///
+    /// `decorators` might contains more information as to the nature of the disruption.
+    pub available: bool,
+    /// Direct URL to product image.
+    pub image_urls: Vec<String>,
+    /// All ingredients in a structured format.
+    pub ingredients: Vec<IngredientInfo>,
+    /// Denotes the nutritional info, normalised to 100g.
+    pub nutritional: Vec<NutritionalInfo>,
+    /// All information for allergy tags.
+    pub allergy_info: Vec<AllergyTags>,
+    /// Denotes all optional bits of information, such as preparation instructions or supplier information.
+    ///
+    /// These can be useful to add as additional collapsable tabs in the front-end ui.
+    pub additional_items: Vec<ItemInfo>,
+    /// All decorators describing the object in further detail.
+    pub decorators: Vec<Decorator>,
+    /// The grocery store this item is provided from.
+    pub provider: Provider,
+}
+
+#[derive(Serialize, Deserialize, async_graphql::SimpleObject, Clone, Debug, PartialEq, PartialOrd)]
+pub struct IngredientInfo {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, async_graphql::SimpleObject, Clone, Debug, PartialEq, PartialOrd)]
+pub struct NutritionalInfo {
+    name: String,
+    value: String,
+    sub_values: Vec<SubNutritionalInfo>,
+}
+
+#[derive(Serialize, Deserialize, async_graphql::SimpleObject, Clone, Debug, PartialEq, PartialOrd)]
+pub struct SubNutritionalInfo {
+    name: String,
+    value: String,
+}
+
+#[derive(Serialize, Deserialize, async_graphql::SimpleObject, Clone, Debug, PartialEq, PartialOrd)]
+pub struct AllergyTags {
+    pub name: String,
+}
+
+/// Contains additional information relevant for an item.
+///
+/// Examples include: Preparation instructions, Supplier info
+#[derive(Serialize, Deserialize, async_graphql::SimpleObject, Clone, Debug, PartialEq, PartialOrd)]
+pub struct ItemInfo {
+    title: String,
+    item_type: Option<ItemType>,
+    text: String,
+}
+
+#[derive(Serialize, Deserialize, async_graphql::Enum, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ItemType {
+    PreparationAdvice,
+    AdditionalInfo,
+    SupplierInfo,
+}
