686a27e97545d4be54cf3114232e7d0c91f396b3
Valentijn Hol <git@hirtol.top>
2022-10-07 02:13:45 +0200

    Add hooks to navigation events to show the progress bar

--- wgg_front/src/lib/components/global_progress/global_loading.ts
+++ wgg_front/src/lib/components/global_progress/global_loading.ts
@@ -16,7 +16,7 @@ export interface GlobalProgress extends Readable<Progress> {
      *
      * Returns the same promise as provided to allow easy chaining.
      */
-    submit<T = never>(promise: Promise<T>): Promise<T>;
+    submit<T = unknown>(promise: Promise<T>): Promise<T>;
 
     /**
      * Manually start the progress bar, needs to be manually stopped with a call to `stop()`.
@@ -30,20 +30,31 @@ export interface GlobalProgress extends Readable<Progress> {
 }
 
 function createGlobalProgressStore(): GlobalProgress {
-    const backing_store: Writable<Promise<never> | undefined> = writable(undefined);
+    const backing_store: Writable<Promise<unknown> | undefined> = writable(undefined);
 
-    const submit = (promise: Promise<never>) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const submit = (promise: Promise<any>) => {
         backing_store.set(promise);
         return promise;
+    };
+    const start = () => {
+        const promise: Promise<unknown> = new Promise((resolver) => (startPromiseResolver = resolver));
+        submit(promise);
+    };
+    const stop = () => {
+        if (startPromiseResolver) {
+            startPromiseResolver({});
+        }
     };
 
     const derived_progress: Readable<Progress> = derived(backing_store, (promise, set) => {
         if (promise != undefined) {
             set(Progress.Begin);
             // Add a small delay to allow the progress bar to move to starting
-            setTimeout(() => set(Progress.Starting), 1);
+            const timeout = setTimeout(() => set(Progress.Starting), 0);
 
             promise.finally(() => {
+                clearTimeout(timeout);
                 backing_store.set(undefined);
             });
         } else {
@@ -56,7 +67,9 @@ function createGlobalProgressStore(): GlobalProgress {
     return {
         subscribe,
         submit,
-        start: () => {},
-        stop: () => {}
+        start,
+        stop
     };
 }
+
+let startPromiseResolver: ((value: unknown) => void) | undefined = undefined;
--- wgg_front/src/routes/+layout.svelte
+++ wgg_front/src/routes/+layout.svelte
@@ -3,30 +3,42 @@
     import '@brainandbones/skeleton/themes/theme-vintage.css';
     import '../app.postcss';
     import { setContextClient } from '$lib/api/urql';
-    import { goto, prefetchRoutes } from '$app/navigation';
+    import { afterNavigate, beforeNavigate, goto, prefetchRoutes } from '$app/navigation';
     import { notifications } from '$lib/components/notifications/notification';
     import { navigating } from '$app/stores';
     import { Spinner } from 'flowbite-svelte';
     import Notifier from '$lib/components/notifications/Notifiers.svelte';
     import GlobalLoading from '$lib/components/global_progress/GlobalLoading.svelte';
+    import { globalLoading } from '$lib/components/global_progress/global_loading';
 
     export let data: LayoutData;
 
     setContextClient(data.client);
 
     prefetchRoutes(['/cart']);
+
+    // 
+    beforeNavigate((nav) => {
+        globalLoading.start();
+    });
+
+    afterNavigate((nav) => {
+        globalLoading.stop();
+    });
 </script>
 
-<GlobalLoading />
+<svelte:head>
+    <title>Wgg</title>
+</svelte:head>
 
 <!-- {#if $navigating}
     <Spinner size="10" class="absolute left-[50%] top-[50%]" />
 {/if} -->
 
+<!-- TODO: NAVIGATING GLOBAL LOADER -->
+<!-- Shows a loader bar at the top of the page similar to Github -->
+<GlobalLoading />
+<!-- Shows notifications for the user -->
 <Notifier />
-
-<svelte:head>
-    <title>Wgg</title>
-</svelte:head>
 
 <slot />
